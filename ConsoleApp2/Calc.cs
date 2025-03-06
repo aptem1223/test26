5cdec09cdb367b137e67f58d5b3ed7a2c3d90fa0
aptem1223 <sanstop357@gmail.com>
2025-03-06 11:11:13 +0200

    Add files via upload

---
+++ ConsoleApp2/Calc.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Calc
+    {
+        private int age;
+
+        private static int retirementAge = 65;
+
+        public Calc()
+        {
+            age = 0;
+        }
+
+        public Calc(int age)
+        {
+            this.age = age;
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string CheckAge()
+        {
+            if (age >= retirementAge)
+            {
+                return "Человек на пенсии.";
+            }
+            else
+            {
+                int yearsLeft = retirementAge - age;
+                return $"До пенсии осталось {yearsLeft} лет.";
+            }
+        }
+
+        public static int RetirementAge
+        {
+            get { return retirementAge; }
+            set
+            {
+                if (value >= 1 && value <= 100)
+                {
+                    retirementAge = value;
+                }
+                else
+                {
+                    Console.WriteLine("Пенсионный возраст должен быть в пределах от 1 до 100 лет.");
+                }
+            }
+        }
+
+
+        public static string CheckRetirementStatus(Calc person)
+        {
+            if (person.age >= retirementAge)
+            {
+                return "Человек на пенсии.";
+            }
+            else
+            {
+                int yearsLeft = retirementAge - person.age;
+                return $"До пенсии осталось {yearsLeft} лет.";
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Calc calc1 = new Calc(60);
+            Calc calc2 = new Calc(70);
+            Console.WriteLine(calc1.CheckAge());
+            Console.WriteLine(calc2.CheckAge());
+            Console.WriteLine(Calc.CheckRetirementStatus(calc1));
+            Console.WriteLine(Calc.CheckRetirementStatus(calc2));
+            Console.WriteLine("Текущий пенсионный возраст: " + Calc.RetirementAge);
+            Calc.RetirementAge = 60;
+            Console.WriteLine("Новый пенсионный возраст: " + Calc.RetirementAge);
+            Calc.RetirementAge = 120;
+        }
+    }
+}
